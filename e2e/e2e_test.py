b0fcd22e5b04490891ecf07de2bd674d50037f77
Allen Lawrence <allenlawrence94@gmail.com>
2019-06-17 15:38:31 -0700

    fix(e2e): e2e tests healthz instead of index

--- e2e/e2e_test.py
+++ e2e/e2e_test.py
@@ -4,6 +4,6 @@ import requests
 url = 'http://172.17.0.1:8080/'
 
 
-def test_index():
-    resp = requests.get(url)
+def test_healthz():
+    resp = requests.get(url + 'healthz')
     assert resp.status_code == 200
